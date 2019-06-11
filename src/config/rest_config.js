76dba3b092a5615e2a2a856f9f2a4edbb5337f03
titusmoyo <37632283+titusmoyo@users.noreply.github.com>
2019-06-11 10:10:58 +0100

    Add files via upload
    initial commit

---
+++ src/config/rest_config.js
@@ -0,0 +1,5 @@
+export const  articles_url = "https://newsapi.org/v2/top-headlines/";
+export const language = "en";
+export const category = "general";
+
+export const _api_key = "b971358de21d4af48ae24b5faf06bbfd";
